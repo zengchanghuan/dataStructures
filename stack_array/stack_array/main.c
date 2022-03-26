a997d9f034494bc7ef498fc0427cc3b2d8c99f08
zengchanghuan <zengchanghuan@gmail.com>
2022-03-26 14:09:56 +0800

    数组实现stack

---
+++ stack_array/stack_array/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  stack_array
+//
+//  Created by 曾长欢 on 2022/3/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
