57dcbe91fe0776b4fb77277343f62dc383d19201
zengchanghuan <zengchanghuan@gmail.com>
2022-03-26 09:25:40 +0800

    remove useless file

--- PublicBlePlugin.h
+++
@@ -1,7 +0,0 @@
-#import <Flutter/Flutter.h>
-#import <CoreBluetooth/CoreBluetooth.h>
-
-@interface PublicBlePlugin : NSObject<FlutterPlugin>
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-
-@end
